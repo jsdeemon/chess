2e4679d855c23ce14616c53d4a90411e069764d2
jsdeemon <jsdeemon@gmail.com>
2022-05-15 20:37:38 +0500

    Added Board and ceil component and basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-
+    <div className="app">
+Work
     </div>
   );
 };
---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BoardComponent = () => {
+  return (
+    <div>BoardComponent</div>
+  )
+}
+
+export default BoardComponent
