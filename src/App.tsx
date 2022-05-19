60ee3147ef91bba2cf2d6add75fae414ed3ba83f
jsdeemon <jsdeemon@gmail.com>
2022-05-19 16:48:03 +0500

    added a board features to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css'
-import BoardComponent from './components/BoardComponent';
+import BoardComponent from './components/BoardComponent'
+import { Board } from './models/Board'
 
 const App = () => {
+
+  const [board, setBoard] = useState(new Board())
+
+  useEffect(() => {
+    restart()
+  }, [])
+
+
+
+  function restart() {
+    const newBoard = new Board()
+    newBoard.initCells()
+    setBoard(newBoard)
+  }
+
   return (
     <div className="app">
 
