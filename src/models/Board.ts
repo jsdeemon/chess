e829303d5ce7e61814145148883c67163f40f399
jsdeemon <jsdeemon@gmail.com>
2022-05-19 16:42:30 +0500

    added some fetures for board and cell

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,4 +1,5 @@
 import { Cell } from "./Cell";
+import { Colors } from "./Colors";
 
 export class Board {
     cells: Cell[][] = [] 
@@ -9,9 +10,9 @@ export class Board {
 
            for (let j = 0; j < 8; j++) {
                if ((i + j) % 2 !== 0) {
-                   row.push(new Cell()) // black cells
+                   row.push(new Cell(this, j, i, Colors.BLACK, null)) // black cells
                } else {
-                   row.push(new Cell()) // white cells
+                   row.push(new Cell(this, j, i, Colors.WHITE, null)) // white cells
                }
            }
            this.cells.push(row) // adding row
