a769aca94f5ba831f9e8d2eaf343a632cacf9b0d
vkclonov <vkclonov@gmail.com>
2022-05-20 15:44:28 +0500

    added constructors for figures

--- src/models/figures/Queen.ts
+++ src/models/figures/Queen.ts
@@ -1,5 +1,16 @@
-import { Figure } from "./Figure";
+import { Cell } from "../Cell";
+import { Colors } from "../Colors";
+import { Figure, FigureNames } from "./Figure";
+import blackLogo from '../../assets/black-queen.png'
+import whiteLogo from '../../assets/white-queen.png'
 
 export class Queen extends Figure {
 
+
+constructor(color: Colors, cell: Cell) {
+    super(color, cell);
+    this.logo = color === Colors.BLACK ? blackLogo : whiteLogo; 
+    this.name = FigureNames.QUEEN;  
+    
+ }
 }
--- src/models/figures/Rook.ts
+++ src/models/figures/Rook.ts
@@ -1,5 +1,16 @@
-import { Figure } from "./Figure";
+import { Cell } from "../Cell";
+import { Colors } from "../Colors";
+import { Figure, FigureNames } from "./Figure";
+import blackLogo from '../../assets/black-rook.png'
+import whiteLogo from '../../assets/white-rook.png'
 
 export class Rook extends Figure {
 
+
+constructor(color: Colors, cell: Cell) {
+    super(color, cell);
+    this.logo = color === Colors.BLACK ? blackLogo : whiteLogo; 
+    this.name = FigureNames.ROOK;  
+    
+ }
 }
