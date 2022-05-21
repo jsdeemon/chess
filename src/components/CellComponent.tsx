307249c49efc490c832a1a76fb80e9d921248db8
jsdeemon <jsdeemon@gmail.com>
2022-05-21 12:44:57 +0500

    added styles and code for cell of figure that availavle for attack

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -10,11 +10,13 @@ interface CellProps {
 const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
   return (
     <div
-    className={['cell', cell.color, selected ? 'selected' : ''].join(' ')}
+    className={['cell', cell.color, selected ? 'selected' : '', cell.available && cell.figure ? 'attack' : ''].join(' ')}
     onClick={() => click(cell)}
     >
+     { cell.available && !cell.figure && <div className="available" />} 
       {cell.figure?.logo && <img src={cell.figure.logo} alt='' /> }
         </div>
+        
   )
 }
 
