f1e8942cd75b6f8b76f550b6ba7d5b8f1692afd9
jsdeemon <jsdeemon@gmail.com>
2022-05-21 12:01:19 +0500

    changed logos of figures

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,6 +1,10 @@
 import { Cell } from "./Cell";
 import { Colors } from "./Colors";
 import { Queen } from "./figures/Queen";
+import { Pawn } from './figures/Pawn';
+import { King } from "./figures/King";
+import { Rook } from "./figures/Rook";
+
 
 export class Board {
     cells: Cell[][] = [] 
@@ -32,19 +36,28 @@ export class Board {
     */
 
     private addPawns() {
-
+        for (let i = 0; i < 8; i++) {
+          new Pawn(Colors.BLACK, this.getCell(i, 1))
+          new Pawn(Colors.WHITE, this.getCell(i, 6))
+            
+        }
     }
 
     private addKings() {
-        
+        new King(Colors.BLACK, this.getCell(4, 0))
+        new King(Colors.WHITE, this.getCell(4, 7))
     }
 
     private addQueens() {
-        
+        new Queen(Colors.BLACK, this.getCell(3, 0))
+        new Queen(Colors.WHITE, this.getCell(3, 7))
     }
 
     private addRooks() {
-        
+        new Rook(Colors.BLACK, this.getCell(0, 0))
+        new Rook(Colors.BLACK, this.getCell(7, 0))
+        new Rook(Colors.WHITE, this.getCell(7, 7))
+        new Rook(Colors.WHITE, this.getCell(0, 7))
     }
 
     private addBishops() {
