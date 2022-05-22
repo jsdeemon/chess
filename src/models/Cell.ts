4f70dce4ecbf61cb703af5263ce06db248db3614
jsdeemon <jsdeemon@gmail.com>
2022-05-22 17:21:50 +0500

    added emptyvertical function

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -21,6 +21,33 @@ export class Cell {
         this.id = Math.random()
     }
 
+    isEmpty() {
+        return this.figure === null 
+    }
+
+    isEmptyVertical(target: Cell): boolean {
+        if (this.x !== target.x) {
+            return false;
+        }
+
+        const min = Math.min(this.y, target.y);
+        const max = Math.max(this.y, target.y);
+        for (let y = min + 1; y < max; y++) {
+            if(this.board.getCell(this.x, y).isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    isEmptyHorizontal(target: Cell): boolean {
+        return true;
+    }
+
+    isEmptyDiagonal(target: Cell): boolean {
+        return true;
+    }
+
     moveFigure(target: Cell) {
         if(this.figure && this.figure?.canMove(target)) {
             this.figure.moveFigure(target)
