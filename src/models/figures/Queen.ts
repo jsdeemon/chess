c24e0af32ddcc99c6c18d81049263878b0fe2dec
jsdeemon <jsdeemon@gmail.com>
2022-05-22 18:07:27 +0500

    added a moveDIAGONAL function to Queen

--- src/models/figures/Queen.ts
+++ src/models/figures/Queen.ts
@@ -21,6 +21,8 @@ constructor(color: Colors, cell: Cell) {
       return true;
    if (this.cell.isEmptyHorizontal(target))
       return true;
+   if (this.cell.isEmptyDiagonal(target))
+      return true;
    return false;
 }
 }
