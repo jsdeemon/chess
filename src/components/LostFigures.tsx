b856b6a1c168995aab855576d6152f596a7894da
jsdeemon <jsdeemon@gmail.com>
2022-05-23 11:28:32 +0500

    added new width and height for each lost figure in component

--- src/components/LostFigures.tsx
+++ src/components/LostFigures.tsx
@@ -12,7 +12,7 @@ const LostFigures: FC<LostFiguresProps> = ({title, figures}) => {
         <h3>{title}</h3>
         {figures.map(figure => 
             <div key={figure.id}>
-                {figure.name} {figure.logo && <img src={figure.logo} /> }
+                {figure.name} {figure.logo && <img width={20} height={20} src={figure.logo} /> }
             </div>
             )}
     </div>
