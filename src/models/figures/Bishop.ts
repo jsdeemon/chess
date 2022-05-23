f43edeb308a6e6071421166a731dffd8c06fe178
jsdeemon <jsdeemon@gmail.com>
2022-05-23 08:08:05 +0500

    added canMove function for Bishop

--- src/models/figures/Bishop.ts
+++ src/models/figures/Bishop.ts
@@ -17,6 +17,8 @@ constructor(color: Colors, cell: Cell) {
  canMove(target: Cell): boolean {
      if (!super.canMove(target))
         return false;
-    return true;
+     if (this.cell.isEmptyDiagonal(target)) 
+        return true;
+    return false;
  }
 }
