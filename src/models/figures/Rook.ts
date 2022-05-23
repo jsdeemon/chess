482b8c457900acc24210941840c0860178521096
jsdeemon <jsdeemon@gmail.com>
2022-05-23 07:57:08 +0500

    added canMove function for Rook

--- src/models/figures/Rook.ts
+++ src/models/figures/Rook.ts
@@ -17,6 +17,11 @@ constructor(color: Colors, cell: Cell) {
  canMove(target: Cell): boolean {
     if (!super.canMove(target))
        return false;
-   return true;
+    if (this.cell.isEmptyVertical(target)) 
+       return true;
+   if (this.cell.isEmptyHorizontal(target)) 
+       return true;
+    
+   return false;
 }
 }
