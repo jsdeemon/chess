9584b37d7dce10dd7a662ce2ea5c92a1a34c9f4f
jsdeemon <jsdeemon@gmail.com>
2022-05-23 08:28:55 +0500

    added canMove function for Pawn

--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -6,6 +6,7 @@ import whiteLogo from '../../assets/white-pawn.png'
 
 export class Pawn extends Figure {
 
+   isFirstStep: boolean = true;
 
 constructor(color: Colors, cell: Cell) {
     super(color, cell);
@@ -17,6 +18,24 @@ constructor(color: Colors, cell: Cell) {
  canMove(target: Cell): boolean {
     if (!super.canMove(target))
        return false;
-   return true;
+   const direction = this.cell.figure?.color === Colors.BLACK ? 1 : -1;
+   const firstStepDirection = this.cell.figure?.color === Colors.BLACK ? 2 : -2;
+
+   // pawn walks straight
+   if ( (target.y === this.cell.y + direction || this.isFirstStep && (target.y === this.cell.y + firstStepDirection)) && target.x === this.cell.x && this.cell.board.getCell(target.x, target.y).isEmpty() ) {
+      return true;
+   }
+
+   if ( target.y === this.cell.y + direction && (target.x === this.cell.x + 1 || target.x === this.cell.x - 1) && this.cell.isEnemy(target)) {
+      return true;
+   }
+
+   return false;
 }
+
+moveFigure(target: Cell): void {
+   super.moveFigure(target);
+   this.isFirstStep = false;
+}
+
 }
